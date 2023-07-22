61b0aa26a5aa5b6680d6195f555d8ba91873f802
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-07-22 19:25:14 +0530

    Add files via upload

---
+++ Program_05/Client.cpp
@@ -0,0 +1,24 @@
+#include "Server.hpp"
+
+int main(void)
+{
+    base bobj;
+    derived dobj;
+
+    bobj.m_biPri = 10; // error as private in base
+    bobj.m_biPro = 20; // error as protected in base
+    bobj.m_biPub = 30; // allowed as public in base
+
+    dobj.m_biPri = 40; // error as private in derived
+    dobj.m_biPro = 50; // error as protected in derived
+    dobj.m_biPub = 60; // allowed as public in derived
+    
+    dobj.m_diPri = 70; // error as private in drived
+    dobj.m_diPro = 80; // error as protected in derived
+    dobj.m_diPub = 90; // allowed as public in derived
+
+    bobj.fun(); // allowed as public in base
+    dobj.fun(); // allowed as public in derived
+
+    return 0;
+}
