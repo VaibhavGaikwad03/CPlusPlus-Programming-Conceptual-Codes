3a18414519bf446fc71a2c4f8c935e660d656a3b
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-09-18 21:11:57 +0530

    Add files via upload

---
+++ First_C++_Program/Server.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class calculation
+{
+    int m_iNo1;
+    int m_iNo2;
+public:
+    calculation();
+    calculation(int iParam1, int iParam2);
+    int addition();
+};
